954cd6656ce3bb6d5f3543b2ac5d069e21188247
pollipogrebnaya <119298212+pollipogrebnaya@users.noreply.github.com>
2023-04-10 10:47:25 +0300

    Update InsectManager.java

--- lab 1/ua/lviv/iot/algo/part 1/lab 1/untitled/src/ua/lviv/iot/algo/part1/lab1/InsectManager.java
+++ lab 1/ua/lviv/iot/algo/part 1/lab 1/untitled/src/ua/lviv/iot/algo/part1/lab1/InsectManager.java
@@ -2,24 +2,25 @@ package ua.lviv.iot.algo.part1.lab2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
-public abstract class InsectManager<T extends Insect> {
-    private List<T> insects = new ArrayList<>();
+public abstract class InsectManager {
+    private List<Insect> insects = new ArrayList<>();
 
-    public void addInsect(T insect) {
+    public void addInsect(Insect insect) {
         insects.add(insect);
     }
 
-    public List<T> findInsectsByNumberOfLegs(int numberOfLegs) {
-        return insects.stream()
-                .filter(insect -> insect.getNumberOfLegs() == numberOfLegs)
+    public List<Insect> findInsectsByNumberOfLegs(int numberOfLegs) {
+        Stream<Insect> insectStream = insects.stream()
+                .filter(insect -> insect.getNumberOfLegs() == numberOfLegs);
+        return insectStream
                 .toList();
     }
 
-    public List<T> findInsectsThatCanInjectPoison() {
+    public List<Insect> findInsectsThatCanInjectPoison() {
         return insects.stream()
                 .filter(Insect::canInjectPoison)
                 .toList();
     }
 }
-
